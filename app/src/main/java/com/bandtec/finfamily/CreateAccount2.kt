8a82e896b4f815089ac290e7734ae9b12a2f60b2
Natalia Santana Borges <nsantanb@everis.com>
2020-03-13 16:14:08 -0300

    Construindo a tela de cadastro com os 3 passos

--- app/src/main/java/com/bandtec/finfamily/CreateAccount2.kt
+++ app/src/main/java/com/bandtec/finfamily/CreateAccount2.kt
@@ -3,16 +3,16 @@ package com.bandtec.finfamily
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import kotlinx.android.synthetic.main.activity_create_account.*
+import kotlinx.android.synthetic.main.activity_create_account2.*
 
-class CreateAccount : AppCompatActivity() {
+class CreateAccount2 : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_create_account)
+        setContentView(R.layout.activity_create_account2)
 
-        buttonnext.setOnClickListener {
-            val intent = Intent(this, CreateAccount2::class.java)
+        buttonnext2.setOnClickListener {
+            val intent = Intent(this, CreateAccount3::class.java)
             // start your next activity
             startActivity(intent)
         }
