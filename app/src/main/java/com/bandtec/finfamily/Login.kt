b6c34cadc32de22fc589398559663fc989293c96
Natalia Santana Borges <nsantanb@everis.com>
2020-03-20 21:32:17 -0300

    Telas de login e cadastro finalizadas + desenvolvimento das telas panel, extract e members

--- app/src/main/java/com/bandtec/finfamily/Login.kt
+++ app/src/main/java/com/bandtec/finfamily/Login.kt
@@ -1,12 +1,22 @@
 package com.bandtec.finfamily
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_login.*
 
 class Login : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        buttonlogin.setOnClickListener {
+            val intent = Intent(this, Group::class.java)
+            // start your next activity
+            startActivity(intent)
+        }
     }
+
+
 }
