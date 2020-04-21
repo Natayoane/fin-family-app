b1ae5c48135a192f33aff632ee38dc482765926e
Natalia Santana Borges <nsantanb@everis.com>
2020-04-21 19:32:28 -0300

    fragment itens da conta

--- app/src/main/java/com/bandtec/finfamily/PopConfirmAction.kt
+++ app/src/main/java/com/bandtec/finfamily/PopConfirmAction.kt
@@ -4,19 +4,21 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_pop_delete_put.*
+import kotlinx.android.synthetic.main.activity_pop_delete_put.btnClose
 import kotlinx.android.synthetic.main.activity_pop_new_invoice.*
-import kotlinx.android.synthetic.main.activity_pop_new_invoice.btnClose
 
-class PopDeletePut : AppCompatActivity() {
+class PopConfirmAction : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_pop_delete_put)
+        setContentView(R.layout.activity_pop_confirm_action)
+
+        val choose = intent.extras?.getInt("choose")
+
+        if (choose == 0)  txtTitle.text = "Confirm delete account"
+        if (choose == 1)  txtTitle.text = "Are you sure you want to leave ?"
 
         btnClose.setOnClickListener {
-            val intent = Intent(this, PopAlterPut::class.java)
-            //start your next activity
-            startActivity(intent)
             finish()
         }
     }
